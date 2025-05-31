86fc3aa8118cc7b000e3ac0de28ec0295497c7eb
Gustavo Pintari <gustavopintari@gmail.com>
2025-05-31 15:48:27 -0300

    notificationConsole

--- force-app/main/default/lwc/notificationConsole/notificationConsole.js
+++ force-app/main/default/lwc/notificationConsole/notificationConsole.js
@@ -1,5 +1,6 @@
 import { LightningElement, track } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { subscribe, unsubscribe, onError } from "lightning/empApi";
 
 export default class NotificationConsole extends LightningElement {
     
@@ -7,18 +8,32 @@ export default class NotificationConsole extends LightningElement {
     notifications = [];
 
     async connectedCallback() {
+        // Configure default error handler for the EMP API
+        onError((error) => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    variant: "error",
+                    title: "EMP API Error",
+                    message: "Check your browser's developer console for more details."
+                })
+            );
+            console.log("EMP API error reported by server: ", JSON.stringify(error));
+        });
+        // Subscribe to our notification platform event with the EMP API
+        // listen to all new events
+        // and handle them with handleNotificationEvent
+        this.subscription = await subscribe("/event/Notification__e", -1, (event) => this.handleNotificationEvent(event));
+        
         this.dispatchEvent(
             new ShowToastEvent({
-                variant: 'success',
-                title: 'Ready to receive notifications'
+                variant: "success",
+                title: "Ready to receive notifications"
             })
         );
+    }
 
-        this.notifications = [
-            { id: 'id1', time: '00:01', message: 'Greetings Trailblazer!' },
-            { id: 'id2', time: '00:02', message: 'Congratulations on building this first version of the app.' },
-            { id: 'id3', time: '00:03', message: 'Beware of the bears.' }
-        ];
+    disconnectedCallback() {
+        unsubscribe(this.subscription);
     }
 
     handleClearClick() {
@@ -27,5 +42,19 @@ export default class NotificationConsole extends LightningElement {
 
     get notificationCount() {
         return this.notifications.length;
+    }
+
+    handleNotificationEvent(event) {
+        console.dir(event);
+        // Parse event data
+        const id = event.data.event.replayId;
+        const message = event.data.payload.Message__c;
+        const utcDate = new Date(event.data.payload.CreatedDate);
+        const time = `${utcDate.getMinutes()}:${utcDate.getSeconds()}`;
+        // Add notification to view
+        const notification = {id, message, time};
+
+        this.notifications.push(notification);
+        this.dispatchEvent(new ShowToastEvent({variant: "info", title: message}));
     }
 }
